8c1f1f5a2407cd294a214d320c1ee59b67ccf4e8
Delmar <reliablesource13@gmail.com>
2023-03-21 15:11:02 -0700

    new stuff

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const bodyparser = require('body-parser');
 const app = express();
 
 app.use(cors());
+
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to my test api');
@@ -11,6 +14,11 @@ app.get('/', (req, res) => {
 app.get('/api', (req, res) => {
     const json = {msg: 'success'};
     res.send(JSON.stringify(json));
+});
+
+app.post('/apiPost', (req, res) => {
+    const data = req.body;
+    console.log(data.msg);
 });
 
 app.listen(8080, () => {
